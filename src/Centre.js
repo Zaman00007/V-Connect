9c7cc66c626084ff4931aa29c6457a5c2b85cacf
Zaman00007 <shahidzaman75724@gmail.com>
2023-12-06 21:36:54 +0530

    Homepage

--- src/Centre.js
+++ src/Centre.js
@@ -4,10 +4,17 @@ import './Centre.css'
 function Centre() {
   return (
     <div className="Post">
-      <h1>Create Invite</h1>
-      <p>Send a new Invitation</p>
-      <button type="submit" className='submit'>Create</button>
+      <div className='Invite'>
+        <h1>Create Invite</h1>
+        <p>Send a new Invitation</p>
+        <button type="submit" className='submit'>Create</button>
+      </div>
+      <div className='Invites'>
+          <h1>Invites</h1>
+      </div>
+
     </div>
+    
   )
 }
 
