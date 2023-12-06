ffbb6391e950f72833df33fad8ca0a532439e76f
Zaman00007 <shahidzaman75724@gmail.com>
2023-12-06 11:04:36 +0530

    Start of the new ERA

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Nav from "./Nav";
-
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Nav className="App-header" />
+      {/* <Home /> */}
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import { IoIosArrowRoundDown } from 'react-icons/io'
+
+
+function Home() {
+  return (
+    <div className='home'>
+      <h1>This is V-Connect Homepage!!!</h1>
+      <IoIosArrowRoundDown className="arrow" />
+    </div>
+  )
+}
+
+export default Home
