f82c9a5564ba80b60082d593d0b6a7e079f6b4e5
Zaman00007 <shahidzaman75724@gmail.com>
2023-12-06 11:51:35 +0530

    Centre

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import "./App.css";
 import Nav from "./Nav";
 import Home from "./Home";
 import Left from "./Left";
+import Centre from "./Centre";
 
 function App() {
   return (
     <div className="App">
-      <Nav className="App-header" />
-      <Left />
-      {/* <Home /> */}
+       <Nav className="App-header" />
+       <div className="Hero">
+         <Left />
+         <Centre />
+       </div>
     </div>
-  );
+
+);
 }
 
 export default App;
---
+++ src/Centre.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Centre() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Centre
