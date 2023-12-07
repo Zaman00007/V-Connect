5d01b303ea33bf25ec349daa4cf39f2b433855ac
Zaman00007 <shahidzaman75724@gmail.com>
2023-12-07 16:11:08 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Nav from "./Nav";
-import Home from "./Home";
 import Left from "./Left";
 import Centre from "./Centre";
 import Right from "./Right";
@@ -12,23 +11,28 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Nav className="App-header" />
+
         <Switch>
           <Route path="/" exact>
-            <Nav className="App-header" />
-
             <div className="Hero">
-              <Left className="Left" />
+              <Left
+                className="Left"
+                name={{ username: 'John', age: 25, gender: 'male', year: 2022 }}
+              />
               <Centre
                 className="Centre"
                 names={[
                   { id: "Sam Desai", event: "Hackathon" },
                   { id: "Rema Thakur", event: "Dance-ball" },
                   { id: "Mansi Bhagoria", event: "Bike-Rider" },
+                  { id: "Ashutosh Dwivedi", event: "Differential Notes" },
                 ]}
               />
               <Right className="Right" />
             </div>
           </Route>
+
           <Route path="/about" exact>
             <About />
           </Route>
