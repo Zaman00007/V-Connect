1e732907461f0ed56fb21648d0fec83f86881aff
Zaman00007 <shahidzaman75724@gmail.com>
2023-12-07 15:05:55 +0530

    Route-error

--- src/App.js
+++ src/App.js
@@ -5,25 +5,36 @@ import Home from "./Home";
 import Left from "./Left";
 import Centre from "./Centre";
 import Right from "./Right";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav className="App-header" />
-      <div className="Hero">
-        <Left className="Left" />
-        <Centre
-          className="Centre"
-          names={[
-            { id: "Sam Desai", event: "Hackathon" },
-            { id: "Rema Thakur", event: "Dance-ball" },
-            { id: "Mansi Bhagoria", event: "Bike-Rider" },
-          ]}
-        />
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" />
+          <Nav className="App-header" />
 
-        <Right className="Right" />
+          <div className="Hero">
+            <Left className="Left" />
+            <Centre
+              className="Centre"
+              names={[
+                { id: "Sam Desai", event: "Hackathon" },
+                { id: "Rema Thakur", event: "Dance-ball" },
+                { id: "Mansi Bhagoria", event: "Bike-Rider" },
+              ]}
+            />
+
+            <Right className="Right" />
+          </div>
+          </Route >
+          <Route path="/about" />
+          <About/>
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Right.js
+++ src/Right.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import './Right.css'
+import React from "react";
+import "./Right.css";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./About";
 
 function Right() {
   return (
-    <div className="Events">
-      <h2>Events</h2>
-    </div>
-  )
+    <Router>
+      <div className="Events">
+        <div className="events-heading">
+          <h2>Events</h2>
+        </div>
+        <div className="about-link">
+          {/* <Link to="/about">
+            <h2>About Us</h2>
+          </Link> */}
+          <Link to="/About">About</Link>
+        </div>
+
+        
+      </div>
+    </Router>
+  );
 }
 
-export default Right
+export default Right;
