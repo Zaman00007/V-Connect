926b8e232e115e390b79ec1eee8c97d3c5b86142
Zaman00007 <shahidzaman75724@gmail.com>
2023-12-07 16:21:18 +0530

    About us

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
 import React from "react";
 import "./App.css";
 import Nav from "./Nav";
+import Home from "./Home";
 import Left from "./Left";
 import Centre from "./Centre";
 import Right from "./Right";
-import About from "./About";
 import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
+import About from "./About";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Nav className="App-header" />
-
         <Switch>
           <Route path="/" exact>
+            <Nav className="App-header" />
+
             <div className="Hero">
-              <Left
-                className="Left"
-                name={{ username: 'John', age: 25, gender: 'male', year: 2022 }}
-              />
+              <Left className="Left" name={{ username: 'John', age: 25, gender: 'male', year: 2022 }} />
               <Centre
                 className="Centre"
                 names={[
                   { id: "Sam Desai", event: "Hackathon" },
                   { id: "Rema Thakur", event: "Dance-ball" },
                   { id: "Mansi Bhagoria", event: "Bike-Rider" },
-                  { id: "Ashutosh Dwivedi", event: "Differential Notes" },
                 ]}
               />
               <Right className="Right" />
             </div>
           </Route>
-
-          <Route path="/about" exact>
+          <Route path="/about" >
             <About />
           </Route>
         </Switch>
