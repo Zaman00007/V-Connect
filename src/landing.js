fbd9c90869999a32bb770c975d97723b62475a45
mansibhagoria <mansibhagoria@gmail.com>
2024-01-19 09:37:23 +0530

    landing

---
+++ src/landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './landing.css'
+
+function landing() {
+  return (
+    
+        <div class="landingContainer">
+      {/* Your landing page content goes here */}
+      <h1 style={{ color: 'white' }}>Welcome to My Website</h1>
+    </div>
+    
+  )
+}
+
+export default landing
