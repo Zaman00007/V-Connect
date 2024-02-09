f32827382378376395cc1be854cb6760b83fad16
Zaman00007 <shahidzaman75724@gmail.com>
2024-02-09 13:17:14 +0530

    Login field

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,19 @@ import { IoIosArrowRoundDown } from 'react-icons/io'
 function Home() {
   return (
     <div className='landing'>
-      <h1 className='head'>Lets Connect!!!</h1>
+      
       {/* <IoIosArrowRoundDown className="arrow" /> */}
       <div className='logo'>
         <img src='./v.png' alt='logo' />
+      </div>
+      
+      <div className='login'>
+      <h1 className='head'>Lets Connect!!!</h1>
+        <form>
+          <input type='text' placeholder='Username' />
+          <input type='password' placeholder='Password' />
+          <button type='submit'>Login</button>
+        </form>
       </div>
     </div>
   )
